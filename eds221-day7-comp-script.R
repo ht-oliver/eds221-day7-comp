e3091c935725d1f4d3486d7be074103c9bb89630
ht-oliver <holiver327@gmail.com>
2025-08-19 11:30:55 -0700

    you are not paying enough attention to your syntax

---
+++ eds221-day7-comp-script.R
@@ -0,0 +1,39 @@
+# Clear environment
+rm(list = ls())
+
+# Attach packages
+library(tidyverse)
+library(palmerpenguins)
+library(lubridate)
+library(dplyr)
+
+# Data wrangling refresher
+
+# 1) only include penguins at Brisco and Dream Islands
+# 2) Remove year and sex variables 
+# 3) Add a new column called body mass in kg with penguin mass converted from grams to kilograms
+# 4) Rename island variable to location
+
+penguins %>%
+  filter(island %in% c("Biscoe", "Dream")) %>% 
+  select(-year, -sex) %>% 
+  mutate("mass_kg" = body_mass_g/1000) %>% 
+  rename(location = island)
+
+# 1. Limit to only Adelie penguins
+# 2. Remove any observations where flipper_length_mm is NA
+# 3. Group the data by sex
+# 4. Find the mean, std, and sample size (n()) of flipper lengths for male and females
+penguins %>% 
+  filter(species == "Adelie") %>%
+  filter(!is.na(flipper_length_mm),
+         !is.na(sex)) %>% 
+  group_by(sex) %>% 
+  summarise(mean = mean(flipper_length_mm),
+            standard_dev = sd(flipper_length_mm),
+            sample_size = n())
+penguins
+
+
+            
+  
